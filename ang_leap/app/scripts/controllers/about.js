3416b2a1ce17e35e51f97fd5465d84a7c0e6c820
manviny <manol@indinet.es>
2015-04-28 19:36:52 +0200

    +

--- ang_leap/app/scripts/controllers/about.js
+++ ang_leap/app/scripts/controllers/about.js
@@ -34,6 +34,10 @@ angular.module('angLeapApp')
 
 	$scope.hand = {};
 	$scope.arm = {};
+	$scope.gesture = {};
+
+	$scope.direccion = {};
+
 	// $scope.hand.direction = {};
 
 
@@ -70,46 +74,88 @@ angular.module('angLeapApp')
 	    // HAND data
 	    if (frame.hands.length > 0) {
 		    for (var i = 0; i < frame.hands.length; i++) {
-		      var hand = frame.hands[i];
-		      $scope.hand.id = hand.id;
-		      $scope.hand.type = hand.type;
-		      $scope.hand.direction = vectorToString(hand.direction, 2);
-		      $scope.hand.palmPosition = vectorToString(hand.palmPosition);
-		      $scope.hand.grabStrength = hand.grabStrength;
-		      $scope.hand.pinchStrength = hand.pinchStrength;
-		      $scope.hand.confidence = hand.confidence;
-		      $scope.arm.direction = vectorToString(hand.arm.direction());
-		      $scope.arm.center = vectorToString(hand.arm.center());
-		      $scope.arm.vector = vectorToString(hand.arm.basis[1]);
 
+				var hand = frame.hands[i];
+				$scope.hand.id = hand.id;
+				$scope.hand.type = hand.type;
+				$scope.hand.direction = vectorToString(hand.direction, 2);
+				$scope.hand.palmPosition = vectorToString(hand.palmPosition);
+				$scope.hand.grabStrength = hand.grabStrength;
+				$scope.hand.pinchStrength = hand.pinchStrength;
+				$scope.hand.confidence = hand.confidence;
+				$scope.arm.direction = vectorToString(hand.arm.direction());
+				$scope.arm.center = vectorToString(hand.arm.center());
+				$scope.arm.vector = vectorToString(hand.arm.basis[1]);
 
-
-
-
-      // handString += "Confidence: " + hand.confidence + "<br />";
-      // handString += "Arm direction: " + vectorToString(hand.arm.direction()) + "<br />";
-      // handString += "Arm center: " + vectorToString(hand.arm.center()) + "<br />";
-      // handString += "Arm up vector: " + vectorToString(hand.arm.basis[1]) + "<br />";
-
-
-			// Hand motion factors
-			if (previousFrame && previousFrame.valid) {
-			    var translation = hand.translation(previousFrame);
-			    var rotationAxis = hand.rotationAxis(previousFrame);
-			    var rotationAngle = hand.rotationAngle(previousFrame);
-			    var scaleFactor = hand.scaleFactor(previousFrame);
-
-			    $scope.$apply(function () {
-			    	$scope.handTranslation = vectorToString(translation); //mm.
-			    	$scope.handRotationAxis = vectorToString(rotationAxis, 2); 
-			    	$scope.handRotationAngle = rotationAngle.toFixed(2); //radians.
-			    	$scope.handScaleFactor = scaleFactor.toFixed(2); 
-			    });
-			}
-
-
+				// Hand motion factors
+				if (previousFrame && previousFrame.valid) {
+				    var translation = hand.translation(previousFrame);
+				    var rotationAxis = hand.rotationAxis(previousFrame);
+				    var rotationAngle = hand.rotationAngle(previousFrame);
+				    var scaleFactor = hand.scaleFactor(previousFrame);
+				    var sensibilidad = 25;
+				     $scope.$apply(function () {
+			        	if(translation[0] < -sensibilidad) { $scope.direccion = {'x':'izquierda'} ; } 
+			        	else if (translation[0] > sensibilidad){ $scope.direccion = {'x':'derecha'} ; }
+			        	if(translation[1] < -sensibilidad) { $scope.direccion = {'z':'abajo'} ; } 
+			        	else if (translation[1] > sensibilidad){ $scope.direccion = {'z':'arriba'} ; }
+			        	if(translation[2] < -sensibilidad) { $scope.direccion = {'y':'adelante'} ; } 
+			        	else if (translation[2] > sensibilidad){ $scope.direccion = {'y':'atras'} ; }
+				     });
+				    $scope.$apply(function () {
+				    	$scope.handTranslation = vectorToString(translation); //mm.
+				    	$scope.handRotationAxis = vectorToString(rotationAxis, 2); 
+				    	$scope.handRotationAngle = rotationAngle.toFixed(2); //radians.
+				    	$scope.handScaleFactor = scaleFactor.toFixed(2); 
+				    });
+				}
 		    }
+		}
 
+
+		// Display Gesture object data
+
+		var gestureString = "";
+		if (frame.gestures.length > 0) {
+
+			if (pauseOnGesture) { togglePause(); }
+
+			for (var i = 0; i < frame.gestures.length; i++) {
+			  var gesture = frame.gestures[i];
+				
+			  switch (gesture.type) {
+			    case "circle":
+
+			      gestureString += "center: " + vectorToString(gesture.center) + " mm, "
+			                    + "normal: " + vectorToString(gesture.normal, 2) + ", "
+			                    + "radius: " + gesture.radius.toFixed(1) + " mm, "
+			                    + "progress: " + gesture.progress.toFixed(2) + " rotations";
+			      break;
+			    case "swipe":
+			      gestureString += "start position: " + vectorToString(gesture.startPosition) + " mm, "
+			                    + "current position: " + vectorToString(gesture.position) + " mm, "
+			                    + "direction: " + vectorToString(gesture.direction, 1) + ", "
+			                    + "speed: " + gesture.speed.toFixed(1) + " mm/s";
+			      break;
+			    case "screenTap":
+			    case "keyTap":
+			      gestureString += "position: " + vectorToString(gesture.position) + " mm";
+			      break;
+			    default:
+			      gestureString += "unkown gesture type";
+			  }
+			  gestureString += "<br />";
+				$scope.$apply(function () {
+			  		$scope.gesture.id = gesture.id;
+			  		$scope.gesture.type = gesture.type;
+			  		$scope.gesture.state = gesture.state;
+			  		$scope.gesture.handIds = gesture.handIds.join(", ");
+			  		$scope.gesture.pointableIds = gesture.pointableIds.join(", ");
+			  		$scope.gesture.duration = gesture.duration+  " &micro;s, ";
+			  		$scope.gestureInfo = gestureString;
+				});
+
+			}
 		}
 
 
